ca760e2fa8a4b86dbc0535b30301bf9712e4b7be
Sandro-it <kozachenkooleksandr@gmail.com>
2024-01-19 18:21:54 +0200

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,40 @@
+const inputElement = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesDiv = document.getElementById("boxes");
+
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const numberOfBoxes = inputElement.value;
+  if (numberOfBoxes < 1 || numberOfBoxes > 100) {
+    alert("Number of elements must be from 1 to 100");
+    return;
+  }
+  destroyBoxes();
+  const boxesArray = createBoxesArray(numberOfBoxes);
+  boxesDiv.append(...boxesArray);
+  inputElement.value = "";
+}
+
+function createBoxesArray(numberOfBoxes) {
+  const boxesArray = [];
+  let size = 30;
+  for (let i = 0; i < numberOfBoxes; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    boxesArray.push(box);
+  }
+  return boxesArray;
+}
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
